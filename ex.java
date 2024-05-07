d911765212173826957ccb052d431a2c1bdbe510
Gee-tha-S <166114149+Gee-tha-S@users.noreply.github.com>
2024-05-07 15:53:50 +0530

    Create ex.java

---
+++ ex.java
@@ -0,0 +1,9 @@
+import java.io.File,
+public static File multipartToFile(MultipartFile file) throws IllegalStateException, IOException {
+    File convFile = new File(file.getOriginalFilename());
+    convFile.createNewFile();
+    FileOutputStream fos = new FileOutputStream(convFile);
+    fos.write(file.getBytes());
+    fos.close();
+    return convFile;
+  }
